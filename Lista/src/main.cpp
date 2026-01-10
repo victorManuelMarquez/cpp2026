c11d431d5292db50798b5c681b066fc718fa3ed4
victor <victormmla3lm@gmail.com>
2026-01-10 12:56:21 -0300

    Lista (clásica) - Se corrigió un error conceptual.

--- Lista/src/main.cpp
+++ Lista/src/main.cpp
@@ -7,41 +7,53 @@ struct Nodo {
     Nodo* siguiente;
 };
 
-void insertar(Nodo*& elemento, int dato);
+struct Lista {
+    int totalItems;
+    Nodo* cabeza;
+};
+
+void insertar(Lista*& lista, int dato);
 int quitar(Nodo*& elemento);
-void mostrar(Nodo*& lista);
-void vaciar(Nodo*& lista);
+void mostrar(Lista*& lista);
+void vaciar(Lista*& lista);
 inline int generar(int min=5, int max=20) { return (rand() % (max - min + 1)) + min; };
 
 int main() {
     std::srand(std::time({}));
-    Nodo* lista = nullptr;
+    Lista* lista = new Lista();
+    lista->cabeza = nullptr;
+    lista->totalItems = 0;
     for (int i=0; i<generar(); i++) {
         insertar(lista, generar(1, 100));
     }
     mostrar(lista);
     vaciar(lista);
+    delete lista;
     return 0;
 }
 
-void insertar(Nodo*& lista, int dato) {
+void insertar(Lista*& lista, int dato) {
     Nodo* nuevo = new Nodo();
     nuevo->dato = dato;
     nuevo->siguiente = nullptr;
-    if (lista == nullptr) {
-        lista = nuevo;
-    } else {
-        Nodo* nodo = lista;
-        while (nodo->siguiente != nullptr) {
-            nodo = nodo->siguiente;
+    if (lista) {
+        Nodo* nodo = lista->cabeza;
+        lista->totalItems++;
+        if (!nodo) {
+            lista->cabeza = nuevo;
+            return;
+        } else {
+            while (nodo->siguiente) {
+                nodo = nodo->siguiente;
+            }
+            nodo->siguiente = nuevo;
         }
-        nodo->siguiente = nuevo;
     }
 }
 
 int quitar(Nodo*& elemento) {
     if (elemento == nullptr) {
-        mostrar(elemento);
+        std::cout << elemento->dato << " quitado de la lista." << std::endl;
         return -1;
     }
     Nodo* temp = elemento;
@@ -51,28 +63,30 @@ int quitar(Nodo*& elemento) {
     return valor;
 }
 
-void mostrar(Nodo*& lista) {
-    int total = 0;
+void mostrar(Lista*& lista) {
     if (!lista) {
-        std::cout << "Lista vacía. ( " << total << " ) elementos." << std::endl;
+        std::cout << "Lista sin iniciar!" << std::endl;
         return;
     }
-    Nodo* nodo = lista;
+    if (!lista->cabeza) {
+        std::cout << "Lista vacía. ( " << lista->totalItems << " ) elementos." << std::endl;
+        return;
+    }
+    Nodo* nodo = lista->cabeza;
     std::cout << "Lista: [ ";
     do {
         std::cout << nodo->dato;
         nodo = nodo->siguiente;
-        total++;
         if (nodo) {
             std::cout << ", ";
         }
     } while (nodo);
-    std::cout << " ] ( " << total << " ) elementos." << std::endl;
+    std::cout << " ] ( " << lista->totalItems << " ) elementos." << std::endl;
 }
 
-void vaciar(Nodo*& lista) {
-    while(lista) {
-        int valor = quitar(lista);
+void vaciar(Lista*& lista) {
+    while(lista->cabeza) {
+        int valor = quitar(lista->cabeza);
         std::cout << valor << " eliminado de la lista." << std::endl;
     }
     mostrar(lista);
