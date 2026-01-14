d4d63d44022d755fe4997aefe6d50c772cecee91
victor <victormmla3lm@gmail.com>
2026-01-14 16:36:27 -0300

    Lista Circular Bidireccional (clásica)

---
+++ ListaCircularBidireccional/src/main.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <cstdio>
+
+struct Nodo {
+    int dato;
+    Nodo* anterior;
+    Nodo* siguiente;
+};
+
+void insertar(Nodo*& nodo, int valor, bool frente = false);
+void mostrar(Nodo*& origen, bool reversa = false);
+int quitar(Nodo*& nodo, bool reversa = false);
+
+int main() {
+    Nodo* lista = nullptr;
+    insertar(lista, 88);
+    insertar(lista, 11, true);
+    insertar(lista, 0);
+    mostrar(lista);
+    mostrar(lista, true);
+    while (lista) {
+        std::cout << lista;
+        int valor = quitar(lista);
+        std::cout << " -> " << valor << " eliminado." << std::endl;
+    }
+    return 0;
+}
+
+void insertar(Nodo*& origen, int valor, bool frente) {
+    Nodo* nuevo = new Nodo();
+    nuevo->dato = valor;
+    nuevo->anterior = nuevo->siguiente = nullptr;
+    if (!origen) {
+        nuevo->anterior = nuevo->siguiente = nuevo;
+        origen = nuevo;
+        return;
+    }
+    if (frente) {
+        nuevo->siguiente = origen;
+        nuevo->anterior= origen->anterior;
+        nuevo->anterior->siguiente = nuevo;
+        nuevo->siguiente->anterior = nuevo;
+        origen = nuevo;
+        return;
+    }
+    Nodo* nodo = origen;
+    while (nodo->siguiente != origen) {
+        nodo = nodo->siguiente;
+    }
+    nuevo->anterior = nodo;
+    nuevo->siguiente = nodo->siguiente;
+    nuevo->anterior->siguiente = nuevo;
+    nuevo->siguiente->anterior = nuevo;
+}
+
+void mostrar(Nodo*& origen, bool reversa) {
+    Nodo* nodo = reversa ? origen->anterior : origen;
+    Nodo* tope = nodo;
+    std::cout << (reversa ? "En reversa: " : "") << "[ ";
+    do {
+        int valor = nodo->dato;
+        printf("(%p <- %p (%d) -> %p)\n", (void*)nodo->anterior, (void*)nodo, valor, (void*)nodo->siguiente);
+        nodo = reversa ? nodo->anterior : nodo->siguiente;
+    } while (nodo != tope);
+    std::cout << "]" << std::endl;
+}
+
+int quitar(Nodo*& nodo, bool reversa) {
+    if (!nodo) {
+        std::cout << "Lista vacía!" << std::endl;
+        return -1;
+    }
+    int valor = nodo->dato;
+    Nodo* ant = nodo->anterior;
+    Nodo* sig = nodo->siguiente;
+    if (ant == nodo && sig == nodo) {
+        ant = nullptr;
+        sig = nullptr;
+        delete nodo;
+        nodo = nullptr;
+    } else {
+        Nodo* temp = nodo;
+        if (reversa) {
+            nodo = nodo->anterior;
+        } else {
+            nodo = nodo->siguiente;
+        }
+        ant->siguiente = sig;
+        sig->anterior = ant;
+        delete temp;
+    }
+    return valor;
+}
