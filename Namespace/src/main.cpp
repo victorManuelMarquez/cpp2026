f2b3d84d8b61f1a8aa70c25b320b97dae0259424
victor <victormmla3lm@gmail.com>
2026-01-23 09:57:41 -0300

    Se agregaron las operaciones restantes.

--- Namespace/src/main.cpp
+++ Namespace/src/main.cpp
@@ -1,17 +1,33 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
 #include "aritmetica.hpp"
 
 // alias para simplificar o describir los módulos importados
 namespace calculo = Aritmetica;
 
+inline int generar(int max = 100) { return rand() % max + 1; };
+
 int main() {
-    std::cout << "suma: 10 + 23 = " << calculo::suma(10, 23) << std::endl;
-    std::cout << "División: 4 / 0 = ";
+    std::srand(std::time({}));
+    int x = generar();
+    int y = generar();
+    std::cout << "suma: " << x << " + " << y << " = " << calculo::suma(x, y) << std::endl;
+    x = generar();
+    y = generar();
+    std::cout << "suma: " << x << " - " << y << " = " << calculo::resta(x, y) << std::endl;
+    x = generar();
+    y = generar();
+    std::cout << "División: " << x << " / " << y << " = ";
     try {
-        std::cout << calculo::division(4, 0) << std::endl;
+        std::cout << std::fixed << std::setprecision(2) << calculo::division<float>(x, y) << std::endl;
     } catch (const std::runtime_error& e) {
         std::cerr << e.what() << std::endl;
     }
+    x = generar();
+    y = generar();
+    std::cout << "suma: " << x << " * " << y << " = " << calculo::producto(x, y) << std::endl;
     std::cout << "Fin del programa." << std::endl;
     return 0;
 }
