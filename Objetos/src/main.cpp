00febd68f19493ee3a93d129a4c8f949fc819338
victor <victormmla3lm@gmail.com>
2026-01-26 12:15:20 -0300

    Polimorfismo

--- Objetos/src/main.cpp
+++ Objetos/src/main.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
+#include <vector>
 #include <persona.hpp>
 #include <empleado.hpp>
 
 int main() {
-    Persona persona("Juan", "Perez");
-    std::cout << persona.info() << std::endl;
-    Empleado empleado("Ana", "Sosa", "Secretaria");
-    std::cout << empleado.info() << std::endl;
+    // vector para almacenar la colección de Persona
+    std::vector<Persona*> gente;
+
+    // instancio las clases
+    gente.push_back(new Persona("Juan", "Perez"));
+    gente.push_back(new Empleado("Ana", "Lopez", "Ejecutivo"));
+    gente.push_back(new Empleado("Lucía", "Cruz", "Secretaria"));
+
+    // Polimorfismo en acción
+    for (Persona* p : gente) {
+        std::cout << p->info() << std::endl;
+    }
+
+    // elimino las instancias
+    for (Persona* p : gente) {
+        delete p;
+    }
+    std::cout << "Fin del programa." << std::endl;
     return 0;
 }
